74f217b7358f5b979103c92b8d57a55ab90e04d8
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-04-28 23:46:17 +0300

    build: add solution and project files for EF Core CRM

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EF_Core_CRM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
