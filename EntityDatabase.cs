621fbf2d63b55a43a74b298529d876222727b462
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-04-30 23:56:53 +0300

    feat: added ef core database class with sql connection string

---
+++ EntityDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM
+{
+    class EntityDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.
+                UseSqlServer("Server=localhost\\SQLEXPRESS;Database=NewTestDatabase;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
