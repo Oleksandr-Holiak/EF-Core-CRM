3dff954aeca7d318b9cd8c161925d01877483760
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-11 18:34:42 +0300

    feat(Customer): Added new table

---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Adress { get; set; }
+        public required int OwnerId { get; set; }
+    }
+}
