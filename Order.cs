307042e8a1f1c6c36721540c6a319ac62251cfe0
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-11 21:12:16 +0300

    feat(Order): New table added

---
+++ Order.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public required int CustomerId { get; set; }
+        public required int SalesRepresentativeId { get; set; }
+        public required DateTime OrderDate { get; set; }
+        public required decimal TotalAmount { get; set; }
+
+    }
+}
