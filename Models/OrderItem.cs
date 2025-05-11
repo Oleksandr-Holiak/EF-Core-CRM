6c510befd4f8df517e34436c799dccaef41e3a30
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-11 23:15:00 +0300

    feat(OrderItem): Navigation properties added

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -11,7 +11,9 @@ namespace EF_Core_CRM.Models
     {
         public int Id { get; set; }
         public required int OrderId { get; set; }
+        public Order Order { get; set; }
         public required int ProductId { get; set; }
+        public Product Product { get; set; }
         public required int Quantity { get; set; }
         public required decimal UnitPrice { get; set; }
         public required decimal LineTotal { get; set; }
