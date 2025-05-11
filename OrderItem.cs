3aaafc3c3fa424a126e26f73657587faa4a5fdad
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-11 21:21:11 +0300

    feat(OrderItem): New table added

---
+++ OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM
+{
+    class OrderItem
+    {
+        public int Id { get; set; }
+        public required int OrderId { get; set; }
+        public required int ProductId { get; set; }
+        public required int Quantity { get; set; }
+        public required decimal UnitPrice { get; set; }
+        public required decimal LineTotal { get; set; }
+    }
+}
