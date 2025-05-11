176fecc81d1b59682395c367267c36bec25f205f
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-11 18:39:22 +0300

    feat(Product): Added new table

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required int Price { get; set; }
+        public required int Stock { get; set; }
+    }
+}
