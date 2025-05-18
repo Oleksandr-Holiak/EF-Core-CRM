b08d763a53f978a512b842f9cd4148fba6c1549b
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-18 15:23:00 +0300

    feat(IMenu): Interface for UI created

---
+++ UI/IMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM.UI
+{
+    public interface IMenu
+    {
+        Task<IMenu> Show();
+    }
+}
