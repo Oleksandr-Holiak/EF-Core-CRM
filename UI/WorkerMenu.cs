b305252072777e20170be97ebf9087e7e0893a3d
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-18 17:43:30 +0300

    feat(Worker menu): Worker menu added

---
+++ UI/WorkerMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM.UI
+{
+    class WorkerMenu : IMenu
+    {
+        public async Task Show()
+        {
+            Console.WriteLine(
+                "Worker menu\n" +
+                "1.Create new order\n" +
+                "2.Edit order\n" +
+                "3.Track your customer's orders\n" +
+                "4.Create new user");
+        }
+        public async Task<IMenu> Get(string input)
+        {
+            int result = input.ParseToInt();
+
+            switch (result)
+            {
+                case 1:
+                    return this;
+                case 2:
+                    return this;
+                case 3:
+                    return this;
+                case 4:
+                    return this;
+                default:
+                    return this;
+            }
+        }
+    }
+}
