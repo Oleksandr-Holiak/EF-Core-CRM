dcb9e4eeb9d04fb7d350099cf4e1a796b8c58512
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-18 18:42:44 +0300

    feat(UI): Customer menu added

---
+++ UI/CustomerMenu.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_CRM.UI
+{
+    class CustomerMenu : IMenu
+    {
+        public async Task Show()
+        {
+            Console.WriteLine(
+                "Customer menu\n" +
+                "1. Track my orders\n" +
+                "2. Log out and exit to main menu");
+        }
+        public async Task<IMenu> Get(string input)
+        {
+            int result = input.ParseToInt();
+
+            switch (result)
+            {
+                case 1:
+                    throw new NotImplementedException();
+                case 2:
+                    return new MainMenu();
+                default:
+                    ConsoleHelper.WriteError("Wrong selection");
+                    return this;
+            }
+        }
+    }
+}
