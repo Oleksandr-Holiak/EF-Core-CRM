1e669177952badb3c0094f09018963d52b52648f
SASHALIK1 <alexsun2006.ukr@gmail.com>
2025-05-18 21:37:49 +0300

    feat(db): migration "Tables name changed and renamed property in Customer table"

---
+++ Migrations/20250518183626_Tables name changed and renamed property in Customer table.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core_CRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class TablesnamechangedandrenamedpropertyinCustomertable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Customer",
+                newName: "FullName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Customer",
+                newName: "Name");
+        }
+    }
+}
